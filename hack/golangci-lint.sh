c8e771a909f60e4b9bc9924411fe1a05165c63da
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-09-23 14:10:15 +0000

    Update dependency golangci/golangci-lint to v1.61.0

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -15,7 +15,7 @@ fi
 export SOURCE_PATH="$(readlink -f "$SOURCE_PATH")"
 
 # renovate: datasource=github-releases depName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION=v1.59.1
+GOLANGCI_LINT_VERSION=v1.61.0
 
 GOLANGCI_LINT_ADDITIONAL_FLAGS=${GOLANGCI_LINT_ADDITIONAL_FLAGS:-""}
 
