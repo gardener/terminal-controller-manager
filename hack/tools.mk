abf186eb65a201d35e5daebb06814686ffd7f264
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-07-17 11:47:38 +0000

    Update dependency securego/gosec to v2.28.0

--- hack/tools.mk
+++ hack/tools.mk
@@ -13,7 +13,7 @@ SETUP_ENVTEST              := $(TOOLS_BIN_DIR)/setup-envtest
 
 # default tool versions
 # renovate: datasource=github-releases depName=securego/gosec
-GOSEC_VERSION ?= v2.27.1
+GOSEC_VERSION ?= v2.28.0
 
 # tool versions from go.mod
 CONTROLLER_GEN_VERSION ?= $(call version_gomod,sigs.k8s.io/controller-tools)
